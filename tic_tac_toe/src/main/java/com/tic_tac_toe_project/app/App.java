87bdedc88af8e31d6b5c8dd7264c1185fe864714
pktech_dev <pktech_dev@pktechdevsipro.mynetworksettings.com>
2024-08-11 17:54:26 -0400

    new file:   .metadata/.lock 	new file:   .metadata/.log 	new file:   .metadata/.mylyn/repositories.xml.zip 	new file:   .metadata/.plugins/org.eclipse.core.resources/.projects/.org.eclipse.egit.core.cmp/.location 	new file:   .metadata/.plugins/org.eclipse.core.resources/.projects/.org.eclipse.egit.core.cmp/.markers.snap 	new file:   .metadata/.plugins/org.eclipse.core.resources/.projects/.org.eclipse.egit.core.cmp/.syncinfo.snap 	new file:   .metadata/.plugins/org.eclipse.core.resources/.root/.indexes/properties.index 	new file:   .metadata/.plugins/org.eclipse.core.resources/.root/.markers.snap 	new file:   .metadata/.plugins/org.eclipse.core.resources/.safetable/org.eclipse.core.resources 	new file:   .metadata/.plugins/org.eclipse.core.resources/0.snap 	new file:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.core.resources.prefs 	new file:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.egit.core.prefs 	new file:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.jdt.ui.prefs 	new file:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.jst.j2ee.webservice.ui.prefs 	new file:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.mylyn.context.core.prefs 	new file:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.mylyn.monitor.ui.prefs 	new file:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.mylyn.tasks.ui.prefs 	new file:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.navigator.prefs 	new file:   .metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.prefs 	new file:   .metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi 	new file:   .metadata/.plugins/org.eclipse.egit.core/.org.eclipse.egit.core.cmp/.project 	new file:   .metadata/.plugins/org.eclipse.egit.core/.org.eclipse.egit.core.cmp/.settings/org.eclipse.core.resources.prefs 	new file:   .metadata/.plugins/org.eclipse.jdt.core/variablesAndContainers.dat 	new file:   .metadata/.plugins/org.eclipse.m2e.core/workspaceState.ser 	new file:   .metadata/.plugins/org.eclipse.oomph.setup/workspace.setup 	new file:   .metadata/.plugins/org.eclipse.ui.intro/introstate 	new file:   .metadata/.plugins/org.eclipse.ui.workbench/workingsets.xml 	new file:   .metadata/version.ini 	deleted:    arawak_tic_tac.txt 	new file:   tic_tac_toe/pom.xml 	new file:   tic_tac_toe/src/main/java/com/tic_tac_toe_project/app/App.java 	new file:   tic_tac_toe/src/test/java/com/tic_tac_toe_project/app/AppTest.java

---
+++ tic_tac_toe/src/main/java/com/tic_tac_toe_project/app/App.java
@@ -0,0 +1,13 @@
+package com.tic_tac_toe_project.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tic_tac_toe/src/test/java/com/tic_tac_toe_project/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.tic_tac_toe_project.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
